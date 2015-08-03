ad1b3e5f2916aeca81b7a1d22412848910af7fe7
Andrés Pereira <apereira@alabs.es>
2015-08-03 13:23:40 +0200

    [deploy] capistrano cleanup config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,5 @@ role :web, %w{capistrano@tramites.alabs.org}
 role :db,  %w{capistrano@tramites.alabs.org}
 
 set :rvm_ruby_version, '2.2.2'
-set :repo_url, 'git@git.alabs.org:carchi/tramites.git'
 set :branch, :staging
 set :deploy_to, '/var/www/tramites.alabs.org'
-
