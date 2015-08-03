7e96be448dbbbffcc0c2118407d365ec2c98dd24
Andrés Pereira <apereira@alabs.es>
2015-08-03 13:23:16 +0200

    [deploy] error tracking with errbit/airbrake clone - typo

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
-  config.api_key = Rails.appplication.secrets.errbit[:api_key]
-  config.host    = Rails.appplication.secrets.errbit[:host]
+  config.api_key = Rails.application.secrets.errbit[:api_key]
+  config.host    = Rails.application.secrets.errbit[:host]
   config.port    = 443
   config.secure  = config.port == 443
 end
