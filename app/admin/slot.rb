1b34356037c19d79e27c49f1c3a0ae610cf0eda8
Andrés Pereira <apereira@alabs.es>
2015-08-26 13:10:41 -0500

    [fix] slot edit form missing hint

--- app/admin/slot.rb
+++ app/admin/slot.rb
@@ -24,9 +24,9 @@ ActiveAdmin.register Slot do
   form do |f|
     f.inputs "Inscripción" do
       f.input :dow, as: :select, collection: Slot::DOW.to_a, hint: "Día de la semana"
-      f.input :starts_hour, "Hora en la que inicia. Formato: '0800'" 
-      f.input :ends_hour, "Hora en la que termina. Formato: '1600'"
-      f.input :time, "Tiempo que se corresponde en minutos. Formato: '30'"
+      f.input :starts_hour, hint: "Hora en la que inicia. Formato: '0800'" 
+      f.input :ends_hour, hint: "Hora en la que termina. Formato: '1600'"
+      f.input :time, hint: "Tiempo que se corresponde en minutos. Formato: '30'"
     end
     f.actions
   end
