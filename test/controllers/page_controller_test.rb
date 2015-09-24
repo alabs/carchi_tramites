15241993a458c3e703419b789f2d92a8c70162d5
Andrés Pereira <apereira@alabs.es>
2015-09-23 21:32:26 -0500

    [tests] fixing tests all passing OK

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -2,21 +2,16 @@ require "test_helper"
 
 class PageControllerTest < ActionController::TestCase
 
-  def test_juventud
-    get :juventud
-    assert_response :success
-  end
-
   def test_audiencia
-    event = FactoryGirl.create(:event, :audiencia)
+    FactoryGirl.create(:event, :audiencia)
     get :audiencia
-    assert_response :success
+    assert_response :redirect
   end
 
   def test_medio_ambiente
-    event = FactoryGirl.create(:event, :plantas)
+    FactoryGirl.create(:event, :plantas)
     get :medio_ambiente
-    assert_response :success
+    assert_response :redirect
   end
 
 end
