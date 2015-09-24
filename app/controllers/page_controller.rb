b93e69930a3d6252d6a85b23db1565cf6c4dcc2a
Andrés Pereira <apereira@alabs.es>
2015-09-23 20:59:08 -0500

    [pages] removing two intermediate pages (/audiencia and /medio-ambiente), redirecting

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -7,12 +7,24 @@ class PageController < ApplicationController
   end
 
   def audiencia
+    ev_audie = Event.type_audience.first
+    if ev_audie
+      redirect_to new_inscription_url(event_id: ev_audie)
+    else
+      redirect_to root_path
+    end 
   end
 
   def juventud
   end
 
   def medio_ambiente
+    ev_trees = Event.type_trees.first
+    if ev_trees
+      redirect_to new_inscription_url(event_id: ev_trees)
+    else
+      redirect_to root_path
+    end 
   end
 
 end
